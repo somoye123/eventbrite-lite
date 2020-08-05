6af06caab64b54d9d424a30ece34a1ff52cc8d3a
Somoye123 <somoye.ayotunde@gmail.com>
2020-08-05 15:18:40 -0700

    add logging out functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,13 @@ class UsersController < ApplicationController
     def show
         @user = User.find(params[:id])
     end
+
+    def destroy
+        session[:user_id] = nil
+        session[:username] = nil
+        flash[:success] = "You have successfully logged out"
+        redirect_to root_path    
+    end
         
     private
     
