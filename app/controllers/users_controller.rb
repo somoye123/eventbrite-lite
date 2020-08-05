94c75cc6184621d9d794b00e430cc29c49c0dc77
Somoye123 <somoye.ayotunde@gmail.com>
2020-08-05 14:53:08 -0700

    add form where you can sign up and a corresponding show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,29 @@
 class UsersController < ApplicationController
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.create(sign_up_params)
+        if @user.save
+          flash[:notice] = 'Post was created successfully'
+          session[:user_id] = @user.id
+          session[:username] = @user.name
+          redirect_to user_path(@user)
+        else
+          flash[:alert] = 'Woops something went wrong'
+          render 'new'
+        end
+    end
+
+    def show
+        @user = User.find(params[:id])
+    end
+        
+    private
+    
+    def sign_up_params
+    params.require(:user).permit(:name)
+    end
 end
+    
