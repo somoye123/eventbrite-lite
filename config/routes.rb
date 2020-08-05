c05b5e950a2b374388c5b7feeaff3c8e2278020c
Somoye123 <somoye.ayotunde@gmail.com>
2020-08-05 15:36:14 -0700

    add session sign_in to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "users#new"
   resources :users, only: [:new, :create, :show, :destroy]
+  get 'sign_in', to: "sessions#new"
+  post 'sign_in', to: "sessions#create"
 end
