505fe1999865eeb393bb3ac6199241b3189b8983
Somoye123 <somoye.ayotunde@gmail.com>
2020-08-06 11:52:55 -0700

    add the association between the event attendee (also a User) and the event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,5 @@
 class Event < ApplicationRecord
-  belongs_to :creator, class_name: 'User'
+  has_many :invitations, foreign_key: :attended_event
+  has_many :attendees, through: :invitations, source: :attendee
+  belongs_to :creator, class_name: "User"
 end
-
-# has_many :invitations, foreign_key: :attended_event_id
-#   has_many :attenders, through: :invitations, source: :event_attender
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,5 @@
 class User < ApplicationRecord
     has_many :events, foreign_key: :creator, class_name: 'Event' 
+    has_many :invitations, foreign_key: :attendee
+    has_many :attended_events, through: :invitations
 end
-
-
-# has_many :invitations, foreign_key: :event_attender_id
-# has_many :attended_events, through: :invitations
