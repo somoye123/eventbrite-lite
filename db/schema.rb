a26d47424a846c674ba549ba45d1fcbcbc43bb4d
Somoye123 <somoye.ayotunde@gmail.com>
2020-08-06 11:53:53 -0700

    Create and migrate all necessary tables and foreign keys

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_05_204338) do
+ActiveRecord::Schema.define(version: 2020_08_06_184736) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2020_08_05_204338) do
     t.text "description"
     t.datetime "date"
     t.integer "creator_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "invitations", force: :cascade do |t|
+    t.integer "attendee_id"
+    t.integer "attended_event_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
