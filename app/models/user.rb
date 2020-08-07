1ade4b6bd1632ce18fb29eb08eaf336ad3ff61d5
Uduak Essien <acushla4real@gmail.com>
2020-08-07 15:23:15 +0100

    add unit test for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ApplicationRecord
   has_many :invitations, foreign_key: :attendee
   has_many :attended_events, through: :invitations
 
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true
+  validates_uniqueness_of :name
+
 
   scope :past, -> { where(['date < ?', Date.today]) }
   scope :upcoming, -> { where(['date > ?', Date.today]) }
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = User.new(name: 'Uduak')
+  end
+
+  test 'User should be valid' do
+    assert @user.valid?
+  end
+
+  test 'user name should be present' do
+    @user.name = " "
+    assert_not @user.valid?
+  end
+
+  test 'user name should be unique' do
+    @user.save
+    @user2 = User.new(name: 'Uduak')
+    assert_not @user2.valid?
+  end
+
 end
