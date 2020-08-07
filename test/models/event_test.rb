964d710d2d21dc6272367d61c2fa779f51e9f151
Uduak Essien <acushla4real@gmail.com>
2020-08-07 15:39:46 +0100

    add unit test for event

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # validates :name, :description, :location, :date, presence: tru
+  def setup
+    @event = Event.new(name: 'Event1', description: 'Event1 Description here', location: 'Event1 location', date: '2020-08-06, 20:00 ', creator_id: 1)
+  end
+
+  test 'event name should be present' do
+    @event.name = " "
+    assert_not @event.valid?
+  end
+
+  test 'event description should be present' do
+    @event.description = " "
+    assert_not @event.valid?
+  end
+
+  test 'event location should be present' do
+    @event.location = " "
+    assert_not @event.valid?
+  end
 end
