8481fb77d282aa9711a05f1312f2c5032246e9dd
Uduak Essien <acushla4real@gmail.com>
2020-08-07 16:52:26 +0100

    fix rubocop lint error

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 class EventTest < ActiveSupport::TestCase
   # validates :name, :description, :location, :date, presence: tru
   def setup
-    @event = Event.new(name: 'Event1', description: 'Event1 Description here', location: 'Event1 location', date: '2020-08-06, 20:00 ', creator_id: 1)
+    @event = Event.new(name: 'Event1', description: 'Event1 Description here',
+                       location: 'Event1 location', date: '2020-08-06, 20:00 ', creator_id: 1)
   end
 
   test 'event name should be present' do
