2a77cd92b136f4ade9c425212794c6856c78d703
Somoye123 <somoye.ayotunde@gmail.com>
2020-08-06 18:49:09 -0700

    refactored view templates to abstract logic into helper and partial

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+  def created_events(user, name, events, title)
+    render partial: name, locals: { obj: events, title: title } if user.events.size > 0
+  end
+
+  def attended_events(user, name, events, title)
+    if user.attended_events.size > 0
+      render partial: name, locals: { obj: events, title: title } if events.size > 0
+    end
+  end
 end
